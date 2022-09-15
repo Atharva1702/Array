e537d1c6face97786d1086def7a6cf00853963a0
Atharva1702 <90234696+Atharva1702@users.noreply.github.com>
2022-09-15 22:34:50 +0530

    Add files via upload

---
+++ multidimarr.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[10][20],n,m;
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cout<<a[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
